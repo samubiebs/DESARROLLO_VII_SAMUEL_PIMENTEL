38b7447bdab92a644d84455a30854521b6765d27
samubiebs <samubiebs@gmail.com>
2025-09-02 19:04:12 -0600

    Añadido ejercicio de Array Push

---
+++ TALLERES/TALLER_3/array_push.php
@@ -0,0 +1,40 @@
+
+<?php
+// Ejemplo de uso de array_push()
+$frutas = ["Manzana", "Naranja", "Plátano"];
+echo "Array original de frutas:</br>";
+print_r($frutas);
+
+array_push($frutas, "Uva", "Pera");
+echo "</br>Array de frutas después de array_push():</br>";
+print_r($frutas);
+
+$lista = ["Rey", "Edgar", "Bruno"];
+echo "Lista de mis inservibles: </br> ";
+print_r($lista);
+
+array_push($lista, "Yoni", "Theo");
+echo "</br> Lista despues del array push con los nombres agregados: </br>";
+print_r($lista);
+
+// Ejercicio: Crea un array con los nombres de 3 de tus amigos
+// y usa array_push() para añadir 2 amigos más
+$misAmigos = ["Otto", "Darrel", "Malek"]; // Reemplaza esto con tu array de amigos
+array_push($misAmigos, "Pablo", "Chucky"); // Reemplaza las comillas vacías con los nombres de tus amigos
+
+echo "</br>Mis Amixes :</br>";
+print_r($misAmigos);
+
+// Bonus: Usa array_push() con un array de arrays
+$playlist = [
+    ["Bohemian Rhapsody", "Queen"],
+    ["Imagine", "John Lennon"]
+];
+array_push($playlist, ["Billie Jean", "Michael Jackson"], ["Like a Rolling Stone", "Bob Dylan"]);
+
+echo "</br>Mi playlist:</br>";
+foreach ($playlist as $cancion) {
+    echo "- {$cancion[0]} por {$cancion[1]}</br>";
+}
+?>
+      
