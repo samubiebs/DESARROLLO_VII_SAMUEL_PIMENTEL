da7a1160c151fcf6870c6bb9bf9e90f0f146f471
samubiebs <samubiebs@gmail.com>
2025-09-03 18:08:25 -0600

    Añadido ejercicio de rand()

---
+++ TALLERES/TALLER_3/rand.php
@@ -0,0 +1,76 @@
+
+<?php
+
+echo "Número aleatorio: " . rand() . "</br>";
+
+$min = 1;
+$max = 10;
+echo "Número aleatorio entre $min y $max: " . rand($min, $max) . "</br>";
+
+
+function lanzarDado() {
+    return rand(1, 6);
+}
+
+echo "</br>Lanzamiento de dado: " . lanzarDado() . "</br>";
+
+
+$lanzamientos = 10;
+echo "Resultados de $lanzamientos lanzamientos:</br>";
+for ($i = 0; $i < $lanzamientos; $i++) {
+    echo lanzarDado() . " ";
+}
+echo "</br>";
+
+function generarContraseña($longitud = 8) {
+    $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $contraseña = '';
+    for ($i = 0; $i < $longitud; $i++) {
+        $contraseña .= $caracteres[rand(0, strlen($caracteres) - 1)];
+    }
+    return $contraseña;
+}
+
+echo "</br>Contraseña aleatoria: " . generarContraseña() . "</br>";
+echo "Contraseña aleatoria (12 caracteres): " . generarContraseña(12) . "</br>";
+
+
+$frutas = ['manzana', 'banana', 'naranja', 'uva', 'pera'];
+$frutaAleatoria = $frutas[rand(0, count($frutas) - 1)];
+echo "</br>Fruta seleccionada aleatoriamente: $frutaAleatoria</br>";
+
+
+function generarNumerosLoteria($cantidadNumeros, $minimo, $maximo) {
+    $numeros = [];
+    while (count($numeros) < $cantidadNumeros) {
+        $numero = rand($minimo, $maximo);
+        if (!in_array($numero, $numeros)) {
+            $numeros[] = $numero;
+        }
+    }
+    sort($numeros);
+    return $numeros;
+}
+
+$numerosLoteria = generarNumerosLoteria(6, 1, 49);
+echo "</br>Números de lotería generados: " . implode(", ", $numerosLoteria) . "</br>";
+
+
+function simularProbabilidad($probabilidad) {
+    return rand(1, 100) <= $probabilidad;
+}
+
+$intentos = 1000;
+$exitos = 0;
+$probabilidad = 30; 
+
+for ($i = 0; $i < $intentos; $i++) {
+    if (simularProbabilidad($probabilidad)) {
+        $exitos++;
+    }
+}
+
+echo "</br>Simulación de probabilidad ($probabilidad%):</br>";
+echo "Éxitos: $exitos de $intentos intentos (" . ($exitos / $intentos * 100) . "%)</br>";
+?>
+      
